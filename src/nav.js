a5a8b9aa27c2905d022a8b58cacb0c9384e8a394
David Smith <david.smith@weaveability.com>
2020-01-20 09:57:50 +0000

    styling for buttons and hiding when not req

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,27 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
+
+import styles from "./nav-styles";
+
+function getPrevStyles(props) {
+  return props.hasPrevious ? styles.prev : styles.prevHidden;
+}
+
+function getNextStyles(props) {
+  return props.hasNext ? styles.next : styles.nextHidden;
+}
 
 function Nav(props) {
   return (
-    <div>
-      <button onClick={props.onPrevious}>&#10094;</button>
-      <button onClick={props.onNext}>&#10095;</button>
+    <div style={styles.root}>
+      <button style={getPrevStyles(props)} onClick={props.onPrevious}>
+        &#10094;
+      </button>
+      <button style={getNextStyles(props)} onClick={props.onNext}>
+        &#10095;
+      </button>
     </div>
-  )
+  );
 }
 
 Nav.propTypes = {
@@ -15,6 +29,6 @@ Nav.propTypes = {
   onNext: PropTypes.func.isRequired,
   hasPrevious: PropTypes.bool,
   hasNext: PropTypes.bool
-}
+};
 
-export default Nav
+export default Nav;
